f01648c9c4a77fc306b7f5fae3b39863b68905ce
Mostafa Raihan <raihan.invite@gmail.com>
2026-09-14 22:21:30 +0600

    db

--- config/settings.py
+++ config/settings.py
@@ -115,6 +115,21 @@ else:
         }
     }
 
+# DATABASE_URL = os.environ.get('DATABASE_URL')
+
+# if not DATABASE_URL:
+#     raise RuntimeError(
+#         'DATABASE_URL is required. Add your Neon PostgreSQL URL to .env'
+#     )
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=DATABASE_URL,
+#         conn_max_age=600,
+#         ssl_require=True,
+#     )
+# }
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
